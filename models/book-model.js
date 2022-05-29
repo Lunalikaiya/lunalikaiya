7f35a63179dc19aad8148e0887c5c162226a66d4
Lunalikaiya <211560@astanait.edu.kz>
2022-05-29 15:39:16 +0600

    updated

--- models/book-model.js
+++ models/book-model.js
@@ -4,7 +4,7 @@ const bookSchema = new Schema({
     title: {type: String},
     image: {type: String, default: "https://res.cloudinary.com/dluwizg51/image/upload/v1651747098/PROJECTS/no_image_pp3wpw.png"},
     body: {type: String},
-    description: {type: String},
+    url: {type: String},
     date: {type: Date, default: Date.now},
     comments: [{type: Schema.Types.ObjectId, ref: 'BookComment'}]
 })
