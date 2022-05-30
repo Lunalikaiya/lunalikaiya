eaa2b65f93ecbad2afae8e2113eca832fb159925
Lunalikaiya <100081637+Lunalikaiya@users.noreply.github.com>
2022-05-31 02:35:03 +0600

    Update admin-panel.js

--- admin-panel.js
+++ admin-panel.js
@@ -1,6 +1,8 @@
 const AdminBro = require('admin-bro')
 const AdminBroExpress = require('admin-bro-expressjs')
 const AdminBroMongoose = require('admin-bro-mongoose')
+const User = require('./models/user-model')
+const bcrypt = require('bcrypt')
 
 const mongoose = require('mongoose')
 
@@ -13,7 +15,18 @@ const adminBro = new AdminBro({
 const router = AdminBroExpress.buildAuthenticatedRouter(adminBro, {
     authenticate: async (email, password) => {
         try {
-            return email === "aiya" && password === "aiya";
+            const user = await User.findOne({email})
+            let hasRole = false
+            user.roles.forEach(role => {
+                if ("ADMIN".includes(role))
+                    hasRole = true
+            })
+            if (!hasRole) return false
+            if (user) {
+                const matched = await bcrypt.compare(password, user.password)
+                if (matched) return user
+            }
+            return false
         } catch (e) {
             console.log(e)
         }
