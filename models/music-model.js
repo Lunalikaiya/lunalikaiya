13f79f7520e0715e9bbce519f8145301bceab2ea
Lunalikaiya <100081637+Lunalikaiya@users.noreply.github.com>
2022-05-30 23:46:21 +0600

    Update music-model.js

--- models/music-model.js
+++ models/music-model.js
@@ -4,6 +4,7 @@ const musicSchema = new Schema({
     title: {type: String},
     image: {type: String, default: "https://res.cloudinary.com/dluwizg51/image/upload/v1651747098/PROJECTS/no_image_pp3wpw.png"},
     body: {type: String},
+    url: {type: String},
     date: {type: Date, default: Date.now},
     comments: [{type: Schema.Types.ObjectId, ref: 'MusicComment'}]
 
